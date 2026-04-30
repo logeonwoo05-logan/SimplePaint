5a206b6dd4793a4719a2f143ad7af40c11bf4573
Logan Kim <logeonwoo05@gmail.com>
2026-04-30 15:00:19 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SimplePaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblAppName = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            btnOpenFile = new Button();
+            button1 = new Button();
+            btnSaveFile = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("맑은 고딕", 20F, FontStyle.Bold);
+            lblAppName.ForeColor = Color.Blue;
+            lblAppName.Location = new Point(0, 0);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(355, 72);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Simple Paint";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(12, 176);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(472, 200);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(496, 176);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(239, 200);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(741, 176);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(330, 200);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "groupBox3";
+            // 
+            // btnOpenFile
+            // 
+            btnOpenFile.BackColor = Color.FromArgb(255, 255, 192);
+            btnOpenFile.Font = new Font("Microsoft Sans Serif", 15F);
+            btnOpenFile.Location = new Point(1097, 243);
+            btnOpenFile.Name = "btnOpenFile";
+            btnOpenFile.Size = new Size(142, 106);
+            btnOpenFile.TabIndex = 0;
+            btnOpenFile.Text = "열기";
+            btnOpenFile.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(33, 67);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 106);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveFile
+            // 
+            btnSaveFile.BackColor = Color.FromArgb(192, 255, 255);
+            btnSaveFile.Font = new Font("Microsoft Sans Serif", 15F);
+            btnSaveFile.Location = new Point(1257, 245);
+            btnSaveFile.Name = "btnSaveFile";
+            btnSaveFile.Size = new Size(142, 106);
+            btnSaveFile.TabIndex = 3;
+            btnSaveFile.Text = "저장";
+            btnSaveFile.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1424, 1168);
+            Controls.Add(btnSaveFile);
+            Controls.Add(btnOpenFile);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(lblAppName);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblAppName;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Button btnOpenFile;
+        private Button button1;
+        private Button btnSaveFile;
+    }
+}
